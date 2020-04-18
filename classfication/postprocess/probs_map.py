7b98c93c818388ba0a85a4748f59890250708d61
ryan_home <778315181@qq.com>
2020-04-18 16:02:20 +0800

    update probs_map.py

--- classfication/postprocess/probs_map.py
+++ classfication/postprocess/probs_map.py
@@ -1,96 +1,108 @@
-import sys,os
+import sys, os, tqdm
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(BASE_DIR)
-
-import logging,openslide
-import time,glob
+import torch
+import logging, openslide
+import time, glob
 import matplotlib.pyplot as plt
 import numpy as np
-import  pandas as pd
+import pandas as pd
 import torch.nn.functional as F
 from classfication.utils.config import TESTSET
 from classfication.preprocess.wsi_ops import wsi
 from openslide import OpenSlide
-from classfication.utils.metrics import  Metric
+from classfication.utils.metrics import Metric
+
+
 class ProbsMap:
-    def __init__(self,dataset,dataloader,save, model, grid_size, tif_folder=TESTSET):
+    def __init__(self, dataset, dataloader, save, net, grid_size, out_fn, tif_folder=TESTSET):
         '''
         dataset: contains all points for prediction
         probs_size: a dict saving all mask dimensions
         save: save path
         grid_size: grid_size
+        out_fn: outputs --> probs
         '''
         self.dataset = dataset
         self.dataloader = dataloader
         self.save = save
-        self.model = model
+        self.net = net
         self.grid_size = grid_size
         self.tif_folder = tif_folder
         self.queue = []
         self._preprocess()
-    
+        self.out_fn = out_fn
+
     def _preprocess(self):
         '''
         initilize numpy array shape for heatmap
         '''
-        self.dimensions={}
-        for tif in glob.glob(os.path.join(self.tif_folder,'*/*.tif')):
+        self.np_shapes = {}
+        for tif in glob.glob(os.path.join(self.tif_folder, '*/*.tif')):
             slide = openslide.OpenSlide(tif)
             basename = os.path.basename(tif).rstrip('.tif')
-            self.dimensions[basename] = (slide.dimensions[1]//self.grid_size,slide.dimensions[0]//self.grid_size)
+            self.np_shapes[basename] = (slide.dimensions[1] // self.grid_size, slide.dimensions[0] // self.grid_size)
 
     def gen_heatmap(self):
-        '''
+        r'''
         outputs: model output wize shape [N,Channel,row,col]
+        npy file would be saved in self.save as well as log file with stats
         '''
-        TP,FP,TN,FN =0,0,0,0
-        metric=Metric()
-        for (data,targets,indexes) in self.dataloader:
-            #data = Variable(data.cuda(async=True), volatile=True)
-            data, targets = data.cuda(),targets.cpu()
-            outputs = self.model(data).cpu()
-            #print ('---checking out put shape',outputs.shape)
-            #n,channel,h,w=outputs.shape
-            if len(outputs.shape)==4:
-                n, c, h, w=outputs.shape
-            else:
-                n, c = outputs.shape
-                h, w = 1, 1
-            outputs = F.softmax(outputs,dim=1)[:,1]
-            metric.add_data(outputs,targets,indexes)
-            for i in range(outputs.shape[0]):
-                slidename, x, y, label = self.dataset.table.loc[int(indexes[i])]
-                if slidename not in self.queue:
-                    if self.queue !=[]:
-                        save = os.path.join(self.save,self.queue[-1])
-                        np.save(f'{save}.npy',probs_map)
-                    probs_map = np.zeros(self.dimensions[slidename])
-                    self.queue.append(str(slidename))
-                col, row = x//self.grid_size, y//self.grid_size # converted to numpy array,relation row-->_y-->h,col-->_x--->w
-                if w>1 and h>1: # is segmentation results n,c,h,w
-                    probs_map[row-h/2:row+h/2,col-w/2:col+w/2]=outputs[i][1].cpu().data.numpy()
-                else: # is binary classfication results
-                    try:
-                        probs_map[row,col]=outputs[i,1].cpu().data.numpy()
-                    except:
-                        print(slidename, x, y, label,col,row)
-        save = os.path.join(self.save,self.queue[-1])
-        np.save(f'{save}.npy',probs_map)
-        logging.info(f"FP:{metric.FP},TP:{metric.TP},TN:{metric.TN},FN:{metric.FN}")
-        logging.info(f"sensitive:{metric.get_sensitivity()}\taccuracy:{metric.get_accuracy()}\tprecision:{metric.get_precision()}")
-        logging.info(f"F1:{metric.get_F1()}\tspecificity:{metric.get_specificity()}")
+        logfile = open(os.path.join(self.save, 'log.txt'), 'w')
+        TP, FP, TN, FN = 0, 0, 0, 0
+        metric = Metric()
+        self.net.eval()  # freeze BN
+        with torch.no_grad():  # save memory
+            qbar = tqdm.tqdm(self.dataloader, dynamic_ncols=True, leave=False)
+            for (data, targets, indexes) in qbar:
+                # data = Variable(data.cuda(async=True), volatile=True)
+                data, targets = data.cuda(), targets.cpu()
+                outputs = self.net(data).cpu()
+                # print ('---checking out put shape',outputs.shape)
+                # n,channel,h,w=outputs.shape
+                probs = self.out_fn(outputs)
+                metric.add_data(probs, targets, indexes)
+                if len(probs.shape) == 3:
+                    n, h, w = probs.shape
+                elif len(probs.shape) == 1:
+                    n = probs.shape[0]
+                    w, h = None, None
+                probs = probs.numpy()
+                for i in range(n):
+                    slidename, x, y, label = self.dataset.table.loc[int(indexes[i])]
+                    if slidename not in self.queue:
+                        if self.queue != []:
+                            save = os.path.join(self.save, self.queue[-1])
+                            np.save(f'{save}.npy', probs_map)
+                        probs_map = np.zeros(self.np_shapes[slidename])
+                        self.queue.append(str(slidename))
+                    col, row = x // self.grid_size, y // self.grid_size  # converted to numpy array,relation row-->_y-->h,col-->_x--->w
+                    if w and h:  # is segmentation results n,c,h,w
+                        probs_map[int(row - h / 2):int(row + h / 2), int(col - w / 2):int(col + w / 2)] = probs[i]
+                    else:
+                        probs_map[row, col] = probs[i]
+        save = os.path.join(self.save, self.queue[-1])
+        np.save(f'{save}.npy', probs_map)
+        logfile.write(f"FP:{metric.FP},TP:{metric.TP},TN:{metric.TN},FN:{metric.FN}\n")
+        logfile.write(
+            f"sensitive:{metric.get_sensitivity()}\taccuracy:{metric.get_accuracy()}\tprecision1:{metric.get_precision()}\n")
+        logfile.write(
+            f"F1:{metric.get_F1()}\tspecificity:{metric.get_specificity()}\tprecision2:{metric.get_precision2()}\n")
+        logfile.close()
+
 
 class OpHeatmap(object):
     @staticmethod
-    def visualization(heatmap,grid_size:int,mask_slide, origin_slide,save):
-
-        origin = origin_slide.get_thumbnail((origin_slide.dimensions[0]/grid_size,origin_slide.dimensions[1]/grid_size))
+    def visualization(heatmap, grid_size: int, mask_slide, level: int, origin_slide, save=None):
+        origin = origin_slide.get_thumbnail(
+            (origin_slide.dimensions[0] / grid_size, origin_slide.dimensions[1] / grid_size))
         otsu = wsi.otsu(origin_slide, grid_size)
         fig, axes = plt.subplots(1, 4, figsize=(20, 20), dpi=100)
         axes[0].imshow(heatmap)
         axes[0].set_title('Heatmap')
         if mask_slide:
-            mask = mask_slide.get_thumbnail((mask_slide.dimensions[0] / grid_size, mask_slide.dimensions[1] / grid_size))
+            mask = mask_slide.get_thumbnail((mask_slide.level_dimensions[level]))
             mask = np.asarray(mask)[:, :, 0]
         else:
             mask = np.zeros_like(heatmap)
@@ -101,42 +113,43 @@ class OpHeatmap(object):
         axes[3].imshow(otsu)
         axes[3].set_title('Tissue')
         plt.savefig(f'{save}.png')
-        
+
     @staticmethod
-    def pool(heatmap,size:int):
-        rows,cols=heatmap.size()
-        data = np.zeros((rows//size,cols//size))
-        for i in range(rows//size):
-            for j in range(cols//size):
-                data[i,j]=np.max(heatmap[size*i:size*i+size,size*j:size*j+size])
+    def pool(heatmap, size: int):
+        rows, cols = heatmap.size()
+        data = np.zeros((rows // size, cols // size))
+        for i in range(rows // size):
+            for j in range(cols // size):
+                data[i, j] = np.max(heatmap[size * i:size * i + size, size * j:size * j + size])
         return data
 
     @staticmethod
-    def to_csv(heatmap:np.array,start_point:int,interval:int,save:str,threshold=0.5):
-        x_start,y_start = start_point
+    def to_csv(heatmap: np.array, start_point: int, interval: int, save: str, threshold=0.5):
+        x_start, y_start = start_point
         outfile = open(save, 'w')
         if threshold:
-            Xs,Ys = np.where(heatmap>threshold)
+            Xs, Ys = np.where(heatmap > threshold)
         else:
-            Xs,Ys = np.where(heatmap>0)
-        for i,j in zip(Xs,Ys):
-            outfile.write('{:0.5f},{},{}'.format(heatmap[i,j], x_start+i*interval, y_start+j*interval) + '\n')
+            Xs, Ys = np.where(heatmap > 0)
+        for i, j in zip(Xs, Ys):
+            outfile.write('{:0.5f},{},{}'.format(heatmap[i, j], x_start + i * interval, y_start + j * interval) + '\n')
         outfile.close()
 
     @staticmethod
-    def csv_to_heatmap(csvfile:str,size,interval:int,save:str):
+    def csv_to_heatmap(csvfile: str, size, interval: int, save: str):
         heatmap = np.zeros(size)
-        with open(csvfile,'r')as f:
+        with open(csvfile, 'r')as f:
             for i in f.readlines():
-                probs,x,y = i.rstrip().split(',')
-                probs,i ,j=float(probs),float(x)//interval,float(y)//interval
-                heatmap[i,j]=probs
+                probs, x, y = i.rstrip().split(',')
+                probs, i, j = float(probs), float(x) // interval, float(y) // interval
+                heatmap[i, j] = probs
         if save:
-            np.save(save,heatmap)
+            np.save(save, heatmap)
         else:
             return heatmap
 
-    def gt_to_heatmap(csvfile:str,size,interval:int,save:str):
+    @staticmethod
+    def gt_to_heatmap(csvfile: str, size, interval: int, save: str):
         heatmap = np.zeros(size)
         with open(csvfile, 'r')as f:
             for line in f.readlines()[1:]:
@@ -144,6 +157,6 @@ class OpHeatmap(object):
                 label, i, j = float(label), float(y) // interval, float(x) // interval
                 heatmap[int(i), int(j)] = label
         if save:
-            np.save(save,heatmap)
+            np.save(save, heatmap)
         else:
             return heatmap
--- classfication/preprocess/generateMask.py
+++ classfication/preprocess/generateMask.py
@@ -1,121 +1,65 @@
-import os,glob
+import os, glob
 import os.path as osp
+
 # add classfication
 BASE_DIR = osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__))))
 import sys
+
 sys.path.append(BASE_DIR)
 # add ASAP into python environment
 sys.path.append('/opt/ASAP/bin')
 from multiprocessing import Pool
 import multiresolutionimageinterface as mir
 from classfication.utils.config import *
-
-from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor,ALL_COMPLETED
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, ALL_COMPLETED
 import concurrent.futures
-import logging
+import logging, openslide
+import numpy as np
 
-def generate_mask(tif,anno_path,mask_path,camelyon17_type_mask,remove=True):
+def generate_mask(tif, anno_xml, mask_path, camelyon17_type_mask, remove=True):
     reader = mir.MultiResolutionImageReader()
     basename = osp.basename(tif)
     output_path = osp.join(mask_path, basename)
-    samplename=basename.rstrip('.tif')
-    xml_path=osp.join(anno_path, f'{samplename}.xml')
-    if not os.path.exists(xml_path): #检查无效的Mask
-        logging.info(f'check {samplename}.xml,it cannot be load!')
+    samplename = basename.rstrip('.tif')
+    xml_path = osp.join(anno_xml, f'{samplename}.xml')
+    if not os.path.exists(xml_path):  # 检查无效的Mask
         if os.path.exists(output_path) and remove:
             os.system(f'rm {output_path}')
+            logging.info(f'check {samplename}.xml,it cannot be load!removed')
         return 1
-    if osp.exists(output_path): # 跳过已有的Mask
-        logging.info(f'{output_path} already exists, pass')
-        return 1
+    elif osp.exists(output_path):  # 跳过已有的Mask
+        try:
+            mask_slide = openslide.OpenSlide(output_path)  # 检查完整性
+            mask_slide.close()
+            return 1
+        except:
+            logging.info(f'{output_path} broken, pass')
+            os.system(f'rm {output_path}')
     mr_image = reader.open(tif)
     annotation_list = mir.AnnotationList()
     xml_repository = mir.XmlRepository(annotation_list)
     xml_repository.setSource(xml_path)
     xml_repository.load()
     annotation_mask = mir.AnnotationToMask()
-    label_map = {'metastases': 1, 'normal': 2} if camelyon17_type_mask else {'_0': 1, '_1': 1, '_2': 0}
+    label_map = {'metastases': 1, 'normal': 2} if camelyon17_type_mask else {'_0': 1, '_1': 1, '_2': 0, 'Exclusion': 0,
+                                                                             'Tumor': 1, '_2': 0}
     conversion_order = ['metastases', 'normal'] if camelyon17_type_mask else ['_0', '_1', '_2']
     annotation_mask.convert(annotation_list, output_path, mr_image.getDimensions(), mr_image.getSpacing(), label_map,
                             conversion_order)
     logging.info(f'generate mask tif for {basename}')
-    return 0
+    return samplename
 
-if __name__=='__main__':
-    logging.basicConfig(level=logging.INFO,filename=os.path.join(MASK_FOLDER,'log.txt'))
-    train_tifs = glob.glob(os.path.join(TRAINSET,'*/*.tif'))
-    test_tifs = glob.glob(os.path.join(TESTSET,'*/*.tif'))
-    with ProcessPoolExecutor(max_workers=20) as pool:
-        futures= [pool.submit(generate_mask, tif,TRAINANNO,MASK_FOLDER,camelyon17_type_mask) for tif in sorted(train_tifs)] + [pool.submit(generate_mask, tif,TESTSETANNO,MASK_FOLDER,camelyon17_type_mask) for tif in sorted(test_tifs)]
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filename=os.path.join(MASK_FOLDER, 'log.txt'))
+    train_tifs = glob.glob(os.path.join(TRAINSET, '*/*.tif'))
+    test_tifs = glob.glob(os.path.join(TESTSET, '*/*.tif'))
+    with ProcessPoolExecutor(max_workers=10) as pool:
+        futures = [pool.submit(generate_mask, tif, TRAINANNO, MASK_FOLDER, camelyon17_type_mask) for tif in
+                   sorted(train_tifs)] + [
+                      pool.submit(generate_mask, tif, TESTSETANNO, MASK_FOLDER, camelyon17_type_mask) for tif in
+                      sorted(test_tifs)]
     concurrent.futures.wait(futures, return_when=ALL_COMPLETED)
     print(f'test {len(test_tifs)}; train {len(train_tifs)}')
     for future in futures:
-        print(future.result())
-    import os, glob
-    import os.path as osp
-
-    # add classfication
-    BASE_DIR = osp.dirname(osp.dirname(osp.dirname(osp.abspath(__file__))))
-    import sys
-
-    sys.path.append(BASE_DIR)
-    # add ASAP into python environment
-    sys.path.append('/opt/ASAP/bin')
-    from multiprocessing import Pool
-    import multiresolutionimageinterface as mir
-    from classfication.utils.config import *
-    from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, ALL_COMPLETED
-    import concurrent.futures
-    import logging, openslide
-    import numpy as np
-
-
-    def generate_mask(tif, anno_xml, mask_path, camelyon17_type_mask, remove=True):
-        reader = mir.MultiResolutionImageReader()
-        basename = osp.basename(tif)
-        output_path = osp.join(mask_path, basename)
-        samplename = basename.rstrip('.tif')
-        xml_path = osp.join(anno_xml, f'{samplename}.xml')
-        if not os.path.exists(xml_path):  # 检查无效的Mask
-            if os.path.exists(output_path) and remove:
-                os.system(f'rm {output_path}')
-                logging.info(f'check {samplename}.xml,it cannot be load!removed')
-            return 1
-        elif osp.exists(output_path):  # 跳过已有的Mask
-            try:
-                mask_slide = openslide.OpenSlide(output_path)  # 检查完整性
-                mask_slide.close()
-                return 1
-            except:
-                logging.info(f'{output_path} broken, pass')
-                os.system(f'rm {output_path}')
-        mr_image = reader.open(tif)
-        annotation_list = mir.AnnotationList()
-        xml_repository = mir.XmlRepository(annotation_list)
-        xml_repository.setSource(xml_path)
-        xml_repository.load()
-        annotation_mask = mir.AnnotationToMask()
-        label_map = {'metastases': 1, 'normal': 2} if camelyon17_type_mask else {'_0': 1, '_1': 1, '_2': 0,
-                                                                                 'Exclusion': 0, 'Tumor': 1, '_2': 0}
-        conversion_order = ['metastases', 'normal'] if camelyon17_type_mask else ['_0', '_1', '_2']
-        annotation_mask.convert(annotation_list, output_path, mr_image.getDimensions(), mr_image.getSpacing(),
-                                label_map,
-                                conversion_order)
-        logging.info(f'generate mask tif for {basename}')
-        return samplename
-
-
-    if __name__ == '__main__':
-        logging.basicConfig(level=logging.INFO, filename=os.path.join(MASK_FOLDER, 'log.txt'))
-        train_tifs = glob.glob(os.path.join(TRAINSET, '*/*.tif'))
-        test_tifs = glob.glob(os.path.join(TESTSET, '*/*.tif'))
-        with ProcessPoolExecutor(max_workers=10) as pool:
-            futures = [pool.submit(generate_mask, tif, TRAINANNO, MASK_FOLDER, camelyon17_type_mask) for tif in
-                       sorted(train_tifs)] + [
-                          pool.submit(generate_mask, tif, TESTSETANNO, MASK_FOLDER, camelyon17_type_mask) for tif in
-                          sorted(test_tifs)]
-        concurrent.futures.wait(futures, return_when=ALL_COMPLETED)
-        print(f'test {len(test_tifs)}; train {len(train_tifs)}')
-        for future in futures:
-            if future != 1:
-                print(future.result())
+        if future != 1:
+            print(future.result())
